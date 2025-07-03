90d9c061ea05c105baf8890e2f54272322414625
unknown <rrg29102@gmail.com>
2025-07-03 16:02:18 +0530

    first commit

---
+++ Task1.java
@@ -0,0 +1,28 @@
+public class Task1{
+
+       int a=12,b=12,c=13;
+       int redius=12;
+
+      public void addition(){
+            System.out.println(a+b+c);
+      }
+
+      public void areaOfCircle(){
+               System.out.println(3.14*redius*redius);
+        }
+
+
+   	public static void main(String[] args){
+                                
+
+                 Task1 t1=new Task1();
+
+                   t1.addition();
+                   t1.areaOfCircle();
+
+
+          }
+
+}
+          
+              
